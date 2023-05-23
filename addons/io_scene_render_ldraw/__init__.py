818ff47edd73de62320aa8a37fa329cf0934cb51
Trevor SANDY <trevor.sandy@gmail.com>
2023-05-23 17:58:30 +0200

    Render LDraw 1.3.8

--- addons/io_scene_render_ldraw/__init__.py
+++ addons/io_scene_render_ldraw/__init__.py
@@ -41,7 +41,7 @@ bl_info = {
     "name": "LPub3D Render LDraw",
     "description": "Render LDraw model as .png file",
     "author": "Trevor SANDY <trevor.sandy@gmail.com>",
-    "version": (1, 3, 7),
+    "version": (1, 3, 8),
     "blender": (2, 82, 0),
     "location": "Render",
     "warning": "",
