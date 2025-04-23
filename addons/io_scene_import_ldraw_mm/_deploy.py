c1ad721aef49e5aa73687347be0d4d424e9b9339
Trevor SANDY <trevor.sandy@gmail.com>
2025-04-23 04:10:15 +0200

    MM LDraw Blender import, export - update _deploy.py to be more usable

--- addons/io_scene_import_ldraw_mm/_deploy.py
+++ addons/io_scene_import_ldraw_mm/_deploy.py
@@ -1,14 +1,22 @@
 import os
 import sys
-from shutil import copytree, ignore_patterns, rmtree
-
+import pathlib
+from shutil import copytree, rmtree
 from definitions import APP_ROOT
+
 
 version = sys.argv[1]
 addon_name = sys.argv[2]
+type = None
+try:
+    type = sys.argv[3]
+except IndexError as e:
+    ...
 
 app_data_dir = os.path.expandvars(r'%APPDATA%\Blender Foundation\Blender')
 addons_dir = r'scripts\addons'
+if type == "extension":
+    addons_dir = r'extensions\user_default'
 blender_dir = os.path.join(app_data_dir, version, addons_dir)
 target = os.path.join(blender_dir, addon_name)
 
@@ -17,13 +25,38 @@ if not os.path.isdir(blender_dir):
 
 print(f"deploying to {target}")
 
-patterns = {".git", "__pycache__", ".idea", ".test"}
-
 if os.path.isdir(target):
     rmtree(target)
 
-copytree(APP_ROOT, target, dirs_exist_ok=True, ignore=ignore_patterns(*patterns))
 
-pycache_dir = os.path.join(target, "__pycache__")
-if os.path.isdir(pycache_dir):
-    rmtree(pycache_dir)
+# https://stackoverflow.com/a/75453578/30179466
+# without this, items like inc/tmp are not ignored
+def callbackIgnore(paths):
+    def ignoref(directory, contents):
+        arr = []
+        for f in contents:
+            for p in paths:
+                if pathlib.PurePath(directory, f).match(p):
+                    arr.append(f)
+        return arr
+
+    return ignoref
+
+
+patterns = [
+    "__pycache__",
+    ".git",
+    ".idea",
+    ".pytest_cache",
+    ".build",
+    "brickset",
+    "experiments",
+    "inc/tmp",
+    "*.blend1",
+    ".gitattributes",
+    ".gitignore",
+    "_deploy.py",
+    "requirements.txt",
+]
+
+copytree(APP_ROOT, target, dirs_exist_ok=True, ignore=callbackIgnore(patterns))
